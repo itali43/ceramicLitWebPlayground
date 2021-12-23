13b6be93ec16cdb9b70fb62ce5bfa8db822c793a
Elliott <itali43@gmail.com>
2021-12-23 15:29:42 -0600

    decrypt now better and a few other cleaning operations

--- src/app.ts
+++ src/app.ts
@@ -9,21 +9,26 @@ declare global {
 
 let litCeramicIntegration = new Integration()
 
-// const ceramicPromise = litCeramicIntegration._createCeramic()
 let streamID = 'kjzl6cwe1jw1479rnblkk5u43ivxkuo29i4efdx1e7hk94qrhjl0d4u0dyys1au' // test data
 
-// const updateAlert = (status: string, message: string) => {
-//   const alert = document.getElementById('alerts')
+const updateAlert = (status: string, message: string) => {
+  const alert = document.getElementById('alerts')
 
-//   if (alert !== null) {
-//     alert.textContent = message
-//     alert.classList.add(`alert-${status}`)
-//     alert.classList.remove('hide')
-//     setTimeout(() => {
-//       alert.classList.add('hide')
-//     }, 5000)
-//   }
-// }
+  if (alert !== null) {
+    alert.textContent = message
+    alert.classList.add(`alert-${status}`)
+    alert.classList.remove('hide')
+    setTimeout(() => {
+      alert.classList.add('hide')
+    }, 5000)
+  }
+}
+const updateStreamID = (resp: String) => {
+  streamID = resp
+  console.log('you now have this as your streamID', streamID)
+  // @ts-ignore
+  document.getElementById('stream').innerText = resp
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('DOMContent.........')
@@ -31,11 +36,18 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 document.getElementById('readCeramic')?.addEventListener('click', () => {
-  console.log('doing decrypt')
-  const decryp = litCeramicIntegration.readAndDecrypt(streamID)
-  console.log('done decrypt: ', decryp)
-  // @ts-ignore
-  document.getElementById('decryption').innerText = decryp
+  if (document.getElementById('stream') === null) {
+    updateAlert('danger', `Error, please write to ceramic first so a stream can be read`)
+  } else {
+    // @ts-ignore
+    console.log('this is the streamID youre sending: ', streamID)
+    const response = litCeramicIntegration.readAndDecrypt(streamID).then(
+      (value) =>
+        // @ts-ignore
+        (document.getElementById('decryption').innerText = value)
+    )
+    console.log(response)
+  }
 })
 
 // encrypt with Lit and write to ceramic
@@ -43,12 +55,8 @@ document.getElementById('readCeramic')?.addEventListener('click', () => {
 document.getElementById('encryptLit')?.addEventListener('click', function () {
   // @ts-ignore
   const stringToEncrypt = document.getElementById('secret').value
-  console.log('hold on, its coming')
-  const response = litCeramicIntegration.ewAsync(stringToEncrypt).then(
-    (value) =>
-      // @ts-ignore
-      (document.getElementById('stream').innerText = value)
-  )
-  //.encryptAndWrite(stringToEncrypt)
-  console.log('ok here it is?', response)
+  const response = litCeramicIntegration
+    .encryptAndWrite(stringToEncrypt)
+    .then((value) => updateStreamID(value))
+  console.log(response)
 })
